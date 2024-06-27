d3b617f6f1379bc8dfc6da7e0953883ec0a86481
lgrandco <l.grandcoinlaf@gmail.com>
2024-06-27 14:23:50 +0200

    z

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,14 +2,17 @@
 
 int main(void) {
     Fixed a;
-    Fixed const b(Fixed(5.05f) * Fixed(2));
-    Fixed c(3);
-    std::cout << c + Fixed(3) << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
     return 0;
 }
